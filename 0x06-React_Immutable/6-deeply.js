8b88ffbd30bd865d46bd8bc75e87e680c40c3ee6
Arsema-Zeweldi <arsugeb@gmail.com>
2023-02-16 22:33:45 +0300

    all lint fix

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,5 +1,5 @@
-import { Map } from "./node_modules/immutable/dist/immutable";
+import { Map } from './node_modules/immutable/dist/immutable';
 
 export default function mergeDeeplyElements(page1, page2) {
-	return Map(page1).mergeDeep(Map(page2));
+  return Map(page1).mergeDeep(Map(page2));
 }
