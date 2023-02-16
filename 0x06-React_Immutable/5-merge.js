00e0b0c50c158334f3b7435c1f8bcebaaeb103be
Arsema-Zeweldi <arsugeb@gmail.com>
2023-02-16 21:19:05 +0300

    Task 5

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,9 @@
+import { List, Map, merge } from "./node_modules/immutable/dist/immutable";
+
+export default function concatElements(page1, page2) {
+    return List(page1).concat(List(page2));
+};
+
+export default function mergeElements(page1, page2) {
+    return Map(page1).merge(Map(page2));
+};
