5380e0e2bed5274f2ecd66973699a56fc3233ba8
Arsema-Zeweldi <arsugeb@gmail.com>
2023-02-16 20:42:19 +0300

    Task 2

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,5 +1,5 @@
 import { fromJS, getIn } from "./node_modules/immutable/dist/immutable";
 
 export default function accessImmutableObject(object, array) {
-    return getIn(fromJS(object), array)
+    return getIn(fromJS(object), array);
 }
